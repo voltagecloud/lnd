c29d003a5b08eb26bc03699e7daeb6cfa093b8a6
Graham Krizek <graham@krizek.io>
2021-03-16 23:11:13 -0500

    Add support for external SSL providers. Also adds support for ZeroSSL from the beginning

--- cert/tls.go
+++ cert/tls.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"io/ioutil"
+	"strings"
 	"sync"
 )
 
@@ -23,6 +24,10 @@ var (
 		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+	}
+	tlsRSACipherSuites = []uint16{
+		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 	}
 )
 
@@ -72,12 +77,34 @@ func LoadCert(certBytes, keyBytes []byte) (tls.Certificate, *x509.Certificate,
 
 // TLSConfFromCert returns the default TLS configuration used for a server,
 // using the given certificate as identity.
-func TLSConfFromCert(certData tls.Certificate) *tls.Config {
-	return &tls.Config{
-		Certificates: []tls.Certificate{certData},
-		CipherSuites: tlsCipherSuites,
-		MinVersion:   tls.VersionTLS12,
+func TLSConfFromCert(certData []tls.Certificate) *tls.Config {
+	var config *tls.Config
+
+	getCertificate := func(h *tls.ClientHelloInfo) (*tls.Certificate, error) {
+		defaultCertList := []string{"localhost", "127.0.0.1"}
+		for _, host := range defaultCertList {
+			if strings.Contains(h.ServerName, host) {
+				return &certData[0], nil
+			}
+		}
+		return &certData[1], nil
 	}
+
+	if len(certData) > 1 {
+		config = &tls.Config{
+			Certificates:   []tls.Certificate{certData[0]},
+			GetCertificate: getCertificate,
+			CipherSuites:   tlsRSACipherSuites,
+			MinVersion:     tls.VersionTLS12,
+		}
+	} else {
+		config = &tls.Config{
+			Certificates: []tls.Certificate{certData[0]},
+			CipherSuites: tlsCipherSuites,
+			MinVersion:   tls.VersionTLS12,
+		}
+	}
+	return config
 }
 
 // NewTLSReloader is used to create a new TLS Reloader that will be used
